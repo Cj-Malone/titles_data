f0abb6e3347b412f9dbd32efaa363fbcda735318
Cj Malone <Cj-Malone@users.noreply.github.com>
2014-12-09 11:16:49 +0000

    Shut up

--- grabber.sh
+++ grabber.sh
@@ -4,7 +4,7 @@ for ARG in $*
 do
 	HexID=$(echo "obase=16; $ARG" | bc)
 	ID=66acd000-77fe-1000-9115-d802$HexID
-	wget http://marketplace.xbox.com/en-US/X8/Game/$ID
+	wget -q http://marketplace.xbox.com/en-US/X8/Game/$ID
 	valid=$(json_verify < $ID)
 	if [[ $valid == "JSON is valid" ]]; then
 		json_reformat -u < $ID > $ARG.json
